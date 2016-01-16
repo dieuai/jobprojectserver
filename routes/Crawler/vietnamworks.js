c82821ce9b4f043b7d0e0ae189007a62d62f6e3e
CaVang <khietnt93@gmail.com>
2016-01-16 22:21:16 +0700

    Update vietnamworks.js

--- routes/Crawler/vietnamworks.js
+++ routes/Crawler/vietnamworks.js
@@ -4,39 +4,33 @@ var request = require('request');
 var cheerio = require('cheerio');
 var urls = [];
 var Job = require('../../models/Jobs');
+var Log = require('../../models/Logs');
 var check = [];
+var number_job = [];
+var error_job = [];
 
-//start();
-
-function start() {
-    var url = 'http://www.vietnamworks.com/it-hardware-networking-it-software-jobs-i55,35-en/page-';
-    for (var u = 0; u < 100; u++) {
-        viecLam(url, u);
-    }
-}
-
-function viecLam(url, i) {
+function viecLam(url, i, report, next) {
     request(url + i, function (err1, resp1, body) {
         if (!err1 && resp1.statusCode == 200) {
             var $ = cheerio.load(body);
             check.push('1');
-            ////*[@id="job-list"]/div[1]/div/table/tbody/tr[50]/td/div/div[1]/div[2]
-            $('.col-sm-9.col-sm-pull-3').each(function(){
+            $('.col-sm-9.col-sm-pull-3').each(function () {
                 if (urls.indexOf($(this).children('div').first().children('a').attr('href')) == -1) {
                     urls.push($(this).children('div').first().children('a').attr('href'));
                 }
             });
             if (check.length == 100) {
                 console.log(urls.length);//889 - 883
+                report['number_url'] = urls.length;
                 for (var i = 0; i < urls.length; i++) {
-                    getLinks(urls[i]);
+                    getLinks(urls[i], report, urls, next);
                 }
             }
         }
     });
 }
 
-function getLinks(url) {
+function getLinks(url, report, urls, next) {
     var job = {
         page: 'vietnamworks.vn',
         link: '' + url,
@@ -46,11 +40,11 @@ function getLinks(url) {
     request(url, function (err, resp, body) {
         if (!err && resp.statusCode == 200) {
             var $ = cheerio.load(body);
-            $('.job-header-info').each(function(){
+            $('.job-header-info').each(function () {
                 job['job'] = $(this).children('h1').first().text().trim().replace(/(?:\\[rn]|[\r\n]+)+/g, '-').toString();
             });
 
-            $('.company-name.text-lg.block').each(function(){
+            $('.company-name.text-lg.block').each(function () {
                 job['author'] = $(this).text().trim().replace(/(?:\\[rn]|[\r\n]+)+/g, '-').toString();
             });
 
@@ -58,26 +52,57 @@ function getLinks(url) {
                 $('#job-detail').children('div').each(function () {
                     switch ($(this).children('h2').text().trim()) {
                         case 'What You Will Do':
-                            job['description'] = $(this).children('div').text().replace(/(?:\\[rn]|[\r\n]+)+/g, '-').replace(/<(?:.|\n)*?>/gm, '').trim().toString();
+                            job['description'] = $(this).children('div').text().trim().replace(/(?:\\[rn]|[\r\n]+)+/g, '-').trim().replace(/<(?:.|\n)*?>/gm, '').trim().toString();
                             break;
                         case 'What You Are Good At':
-                            job['requirement'] = $(this).children('div').text().replace(/(?:\\[rn]|[\r\n]+)+/g, '-').replace(/<(?:.|\n)*?>/gm, '').trim().toString();
+                            job['requirement'] = $(this).children('div').text().trim().replace(/(?:\\[rn]|[\r\n]+)+/g, '-').trim().replace(/<(?:.|\n)*?>/gm, '').trim().toString();
                             break;
                     }
                 });
             });
-            $('.pull-right.text-gray-light').children().last().each(function(){
+
+            $('.pull-right.text-gray-light').children().last().each(function () {
                 job['date_update'] = (new Date(new Date().setDate(new Date().getDate() - parseInt($(this).text().replace(/([A-Z]|[a-z])+/g, '-').trim()))));
             })
         }
+        else {
+            error_job.push('1');
+        }
         if (Object.keys(job).length > 3) {
+            number_job.push('1');
             var result = new Job(job);
             result.save(function (err) {
                 if (err) return console.error(err);
-                console.log("xong");
             });
         }
-    })
+        else {
+            error_job.push('1');
+        }
+        console.log(number_job.length + error_job.length);
+        if((number_job.length + error_job.length) == urls.length){
+            report['number_job'] = number_job.length;
+            report['error_job'] = error_job.length;
+            report['date'] = new Date();
+            if(Object.keys(report).length == 5){
+                var log = new Log(report);
+                log.save(function(err){
+                    if(err) return console.error(err);
+                    next();
+                });
+            }
+        }
+    });
 }
 
-module.exports = router;
+module.exports = {
+    vietnamworks: function (next) {
+        console.log('1');
+        var report = {
+            page: 'vietnamworks'
+        };
+        var url = 'http://www.vietnamworks.com/it-hardware-networking-it-software-jobs-i55,35-en/page-';
+        for (var u = 0; u < 100; u++) {
+            viecLam(url, u, report, next);
+        }
+    }
+};
