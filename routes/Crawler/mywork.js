5c11c230dcf76e2366dd1ad6cc413831af938258
CaVang <khietnt93@gmail.com>
2016-01-16 22:22:43 +0700

    Update mywork.js

--- routes/Crawler/mywork.js
+++ routes/Crawler/mywork.js
@@ -4,28 +4,23 @@ var request = require('request');
 var cheerio = require('cheerio');
 var urls = [];
 var Job = require('../../models/Jobs');
+var Log = require('../../models/Logs');
 var check = [];
 request = request.defaults({
     jar: true
 });
 var j = request.jar();
+var number_job = [];
+var error_job = [];
 
-//start();
 
-function start() {
-    var durl = ["http://mywork.com.vn/tuyen-dung/37/it-phan-cung-mang.html/trang/", "http://mywork.com.vn/tuyen-dung/38/it-phan-mem.html/trang/", "http://mywork.com.vn/tuyen-dung/39/thiet-ke-do-hoa-web.html/trang/", "http://mywork.com.vn/tuyen-dung/40/thuong-mai-dien-tu.html/trang/"];
-    for (var u = 0; u < durl.length; u++) {
-        templeRun(durl[u]);
-    }
-}
-
-function templeRun(durl) {
+function templeRun(durl, report, next) {
     for (var i = 0; i < 100; i++) {
-        viecLam(durl, i);
+        viecLam(durl, i, report, next);
     }
 }
 
-function viecLam(durl, i) {
+function viecLam(durl, i, report, next) {
     request('' + durl + '' + i + '#list_job', function (err1, resp1, body) {
         if (!err1 && resp1.statusCode == 200) {
             var $ = cheerio.load(body);
@@ -43,18 +38,18 @@ function viecLam(durl, i) {
                     }
                 }
             });
-            console.log(check.length);
             if (check.length == 394) {
                 console.log(urls.length);// 10795 - 1354
+                report['number_url'] = urls.length;
                 for (var i = 0; i < urls.length; i++) {
-                    getLinks(urls[i]);
+                    getLinks(urls[i], report, next);
                 }
             }
         }
     });
 }
 
-function getLinks(url) {
+function getLinks(url, report, next) {
     var job = {
         page: 'mywork.com.vn',
         link: '' + url
@@ -63,17 +58,17 @@ function getLinks(url) {
     request({
         url: url,
         headers: {
-            'Cookie': 'job_watched=%5B%22766708%22%5D; PHPSESSID=1g280k04d80qh0osm9tkt9vot3; lastvisit=1450238000; _gat=1; sessionhash=584fe1ec60bdbe4d4a80f1f507a81c4c; lastactivity=1451044063; __asc=0f34c207151d8f633ac4d308001; __auc=3838ba79151a104c9c045750993; _ga=GA1.3.939673775.1450105425',
+            'Cookie': 'PHPSESSID=arv68upss2vhc8s7ak9at78ru7; lastvisit=1451148885; _gat=1; sessionhash=62a9aed12d47c6b43972f0ec532ee863; lastactivity=1452953032; __asc=3b0b70eb1524abf1891d738e920; __auc=3838ba79151a104c9c045750993; _ga=GA1.3.939673775.1450105425',
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.80 Safari/537.36'
         },
         jar: j
     }, function (err, resp, body) {
         if (!err && resp.statusCode == 200) {
             var $ = cheerio.load(body);
-            $('.fullname-company').each(function(){
+            $('.fullname-company').each(function () {
                 job['author'] = $(this).text().trim().replace(/(?:\\[rn]|[\r\n]+)+/g, '-').toString();
             });
-            $('.address-company.mw-ti').each(function(){
+            $('.address-company.mw-ti').each(function () {
                 job['diachilienhe'] = $(this).text().toString();
             });
             $('.block_info.contentdef.list-desjob-company').each(function () {
@@ -81,12 +76,12 @@ function getLinks(url) {
                     switch ($(this).children().first().text()) {
                         case 'Mức lương':
                             job['salary'] = ($(this).map(function () {
-                                return $(this).text().replace('Mức lương\n            Từ ', '').replace('00,000 đến ', ' - ').replace('00,000 VND', ' triệu').replace(',','.').toString();
+                                return $(this).text().replace('Mức lương\n            Từ ', '').replace('00,000 đến ', ' - ').replace('00,000 VND', ' triệu').replace(',', '.').toString();
                             }).get()[0]).trim();
                             break;
                         case 'Kinh nghiệm':
                             job['exp'] = ($(this).map(function () {
-                                return $(this).text().replace('Kinh nghiệm','').trim().toString();
+                                return $(this).text().replace('Kinh nghiệm', '').trim().toString();
                             }).get()[0]).trim();
                             break;
                         case 'Trình độ học vấn':
@@ -176,20 +171,50 @@ function getLinks(url) {
                     });
                 })
             });
-            $('.action_job.score-job-company').children().first().each(function(){
+            $('.action_job.score-job-company').children().first().each(function () {
                 job['date_update'] = new Date($(this).children().last().map(function () {
                     return $(this).text();
                 }).get()[0].trim().replace('Ngày cập nhật:', '').split(/-/).reverse().join('/'));
             })
         }
+        else {
+            error_job.push('1');
+        }
         if (Object.keys(job).length > 2) {
+            number_job.push('1');
             var result = new Job(job);
             result.save(function (err) {
                 if (err) return console.error(err);
-                console.log("xong");
             });
+        }
+        else {
+            error_job.push('1');
+        }
+        console.log(number_job.length + error_job.length);
+        if((number_job.length + error_job.length) == urls.length){
+            report['number_job'] = number_job.length;
+            report['error_job'] = error_job.length;
+            report['date'] = new Date();
+            if(Object.keys(report).length == 5){
+                var log = new Log(report);
+                log.save(function(err){
+                    if(err) return console.error(err);
+                    next();
+                });
+            }
         }
     })
 }
 
-module.exports = router;
+module.exports = {
+    mywork: function (next) {
+        console.log('4');
+        var report = {
+            page: 'mywork'
+        };
+        var durl = ["http://mywork.com.vn/tuyen-dung/37/it-phan-cung-mang.html/trang/", "http://mywork.com.vn/tuyen-dung/38/it-phan-mem.html/trang/", "http://mywork.com.vn/tuyen-dung/39/thiet-ke-do-hoa-web.html/trang/", "http://mywork.com.vn/tuyen-dung/40/thuong-mai-dien-tu.html/trang/"];
+        for (var u = 0; u < durl.length; u++) {
+            templeRun(durl[u], report, next);
+        }
+    }
+};
