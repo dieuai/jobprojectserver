053ed153afebcb345bff56338ec1698bae3fc8ff
dieuai <vtda144@gmail.com>
2016-01-17 21:23:31 +0700

    Update app.js

--- app.js
+++ app.js
@@ -84,7 +84,9 @@ io.on('connection', function (socket) {
 
 });
 
-http.listen(process.env.PORT || 5000);
+http.listen(process.env.PORT || 5000, function(){
+    console.log('ITJob is running');
+});
 
 // setInterval(function(){
     autoCrawler();
